8e7610bf2342de5dac078e07a6f74a120acde1df
hoholabs <johnpaulhoran@gmail.com>
2022-03-01 20:26:03 -0500

    added wind direction arrow, started on unit changing functionality

--- main.js
+++ main.js
@@ -2,23 +2,49 @@ let input = document.querySelector('input');
 let button = document.querySelector('button');
 
 button.addEventListener('click', ()=>{
-    clearWeather();
     showWeather(getWeather());
 });
 
 input.addEventListener("keyup", ()=> {
 
     if (event.keyCode === 13){
-        clearWeather();
         showWeather(getWeather());
     }
 
 });
 
+let units = "imperial"
+
+let fBtn = document.getElementById('fBtn');
+let cBtn = document.getElementById('cBtn');
+
+fBtn.addEventListener('click', ()=>{
+    units = "imperial";
+    fBtn.style.color = "var(--color2)";
+    fBtn.style.backgroundColor = "var(--color1)";
+
+    cBtn.style.color = "var(--color1)";
+    cBtn.style.backgroundColor = "var(--color2)";
+
+    showWeather(getWeather());
+})
+
+cBtn.addEventListener('click', ()=>{
+    units = "metric";
+    cBtn.style.color = "var(--color2)";
+    cBtn.style.backgroundColor = "var(--color1)";
+
+    fBtn.style.color = "var(--color1)";
+    fBtn.style.backgroundColor = "var(--color2)";
+
+    showWeather(getWeather());
+})
+
+
 async function getWeather(){
 
     //Use geocode API to get lat and lon from input
-    const units = "imperial"
+    
     const locResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=5&appid=127a7807fe4ed3c75fbfbbfde9368206`);
     const location = await locResponse.json();
 
@@ -54,44 +80,74 @@ function convertWind(deg){
 };
 
 async function showWeather(promise){
+    clearWeather();
     let spinner = document.getElementById("spinner");
     spinner.style.display = "inline";
 
     let array = await promise;
     spinner.style.display = "none";
     let report = array[0];
-    let forecast = array[1];
+    //let forecast = array[1];
     console.log(report);
-    console.log(forecast);
+
 
     let temp =  report.main.temp;
-    let perc =  report.weather[0].main;
-    let desc =  report.weather[0].description;
+    let prec =  report.weather[0].main;
+    let precType = prec.toLowerCase();
+    let precAmt = report[precType]["1h"];
+    let desc =  report.weather[0];
     let windSpeed =  report.wind.speed;
     let windDir =  report.wind.deg;
 
     let tempDiv = document.getElementById("temp");
-    let percDiv = document.getElementById("perc");
+    let precDiv = document.getElementById("prec");
     let descDiv = document.getElementById("desc");
     let windDiv = document.getElementById("wind");
 
-    percDiv.textContent = perc
-    tempDiv.textContent = Math.round(temp*10)/10+"°";
-    descDiv.textContent = desc;
-    windDiv.textContent = `${Math.round(windSpeed)}mph\n${convertWind(windDir)}`;
+    //precipitarion
+    precDiv.textContent = prec;
+
+    //Temp
+    tempDiv.textContent = Math.round(temp*1)/1+"°";
+
+    //Description
+    descDiv.textContent = desc.description + precipitation(precAmt);
+    let icon = document.createElement('img');
+    icon.src = `http://openweathermap.org/img/wn/${desc.icon}@2x.png`;
+    descDiv.append(icon);
+
+
+    let windText = document.createElement('span');
+    windText.textContent = `${Math.round(windSpeed)}mph\n${convertWind(windDir)}`;
+
+    let windArrow = document.createElement('div')
+    windArrow.id = 'windArrow';
+    windArrow.style.transform = `rotate(${windDir}deg)`
+
+    windDiv.append(windText);
+    windDiv.append(windArrow);
+
+
 
 }
 
 function clearWeather(){
     let tempDiv = document.getElementById("temp");
-    let percDiv = document.getElementById("perc");
+    let precDiv = document.getElementById("prec");
     let descDiv = document.getElementById("desc");
     let windDiv = document.getElementById("wind");
 
-    percDiv.textContent = ""
+    precDiv.textContent = ""
     tempDiv.textContent = ""
     descDiv.textContent = ""
     windDiv.textContent = ""
+    console.log(windDiv);
 }
 
-//showWeather(getWeather());
+function precipitation(precAmt){
+
+
+    return precAmt
+}
+
+showWeather(getWeather());
